9b729e05ff48719edaea3391be4249aa82526060
Zecharias Mengis <zmengis@gmail.com>
2021-02-16 17:14:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+   min_value = 1000000
+   min_value_key = ""
   name_hash.each do |key, value|
-    min_value = 1000000
-    min_value_key = ""
     if value < min_value
       min_value = value
       min_value_key = key
